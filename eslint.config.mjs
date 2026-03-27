584671616fe20aa115eb022ad53fd90f1ea94635
Shubhankar <shuboyjee1@gmail.com>
2026-03-27 13:57:37 +0530

    pckgupdates and all

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,15 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+// 1. Import it
+import prettierConfig from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  // 2. Add it here (usually last to override others)
+  prettierConfig,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
 
-export default eslintConfig;
+export default eslintConfig
